e98502ecf91b3aec899fd8d95b926bf4489a1e95
Mohsen <m.sedaghatfard@gmail.com>
2025-01-09 09:03:34 +0330

    Create CustomTabBarWithPopupMenuApp.swift

---
+++ CustomTabBarWithPopupMenu/CustomTabBarWithPopupMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarWithPopupMenuApp.swift
+//  CustomTabBarWithPopupMenu
+//
+//  Created by Mohsen on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarWithPopupMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
